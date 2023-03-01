cc00a912996387098dd867f5cf8460f22f1fa162
Roberta30 <107767893+Roberta30@users.noreply.github.com>
2023-02-28 21:30:19 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("V A R I Á V E L", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("F U N Ç Ã O", 700,150)
+    fill("lightblue")
+    text("Dica: Performa uma tarefa em particular!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B A N C O D E D A D O S", 100,250)
+    fill("lightblue")
+    text("Dica: Armazena todas as informações!", 100,270)
+
+}
